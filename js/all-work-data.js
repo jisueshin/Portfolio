d33e51fefab53678d10dd3272425996830402689
jisueshin <126222350+jisueshin@users.noreply.github.com>
2024-04-23 12:21:08 -0400

    added research formatting

---
+++ js/all-work-data.js
@@ -0,0 +1,12 @@
+const allWork = [{
+    "title": "Ethics and Perception of AI-Generated Images in Political Campaigns",
+    "course": "FATE",
+    "date": "Fall 2023",
+    "tags": ["HCI", "U.S. Politics"]
+},
+{
+    "title": "Clinton and Bush Administrations' Approach to Denuclearization of North Korea: a Decision Science Perspective",
+    "course": "Decision Science",
+    "date": "Fall 2022",
+    "tags": ["Intl Relations", "Dec Science"]
+}]
--- js/work.js
+++ js/work.js
@@ -1,16 +1,52 @@
-class Research {
-    constructor(title, blurb){
-        this.title = title;
-        this.blurb = blurb;
+console.log(allWork[0].title)
+
+for (const paper of allWork){
+    let paperTitle = paper.title;
+    let paperCourse = paper.course;
+    let paperTags = paper.tags;
+    addPaper(paperTitle, paperCourse, paperTags);
+    /*addTags(paperTags);*/
+}
+
+function addPaper(paperTitle, paperCourse, paperTags){
+    const template = document.querySelector('#research-template');
+    const clone = template.content.cloneNode(true);
+    let researchBlock = clone.querySelector(".research-block");
+    const researchContainer = document.querySelector(".research-row");
+    researchContainer.append(researchBlock);
+    updatePaper(researchBlock, paperTitle, paperCourse, paperTags);
+}
+
+function updatePaper(researchBlock, paperTitle, paperCourse, paperTags){
+    let researchTitle = researchBlock.querySelector(".research-title");
+    researchTitle.innerText = paperTitle;
+    let researchImg = researchBlock.querySelector(".research-img");
+    researchImg.src = "assets/" + paperCourse + ".jpg";
+    let tagRow = researchBlock.querySelector(".tag-row");
+    for (const tag of paperTags){
+        console.log(tag)
+        let tagDisplay = document.createElement("p");
+        tagDisplay.textContent = tag;
+        tagDisplay.setAttribute("class", "HCI-tag")
+        console.log(tagDisplay)
+        tagRow.appendChild(tagDisplay);
     }
 }
 
-new Research("hello", "hi")
-
+/*function addTags(paperTags){
+    let tagRow = document.querySelector(".tag-row");
+    for (const tag of paperTags){
+        console.log(tag)
+        let tagDisplay = document.createElement("p");
+        tagDisplay.textContent = tag;
+        console.log(tagDisplay)
+        tagRow.appendChild(tagDisplay);
+    }
+}*/
+/*
 const papers = [
     ["Ethics and Perception of AI-generated Images in Political Campaigns", "FATE fall 2023"],
     ["Clinton and Bush Administrations' Approach to Denuclearization of North Korea: a Decision Science Perspective", "hi2"],
-    ["Lynching in Post-Reconstruction America", "blurb"]
 ]
 
 let expanded = false 
@@ -48,5 +84,5 @@ function addPaper(paper, title, courseInfo){
 function closeResearch(){
     let currentPapers = document.querySelectorAll("#work-paper");
     currentPapers.forEach((currPaper) => currPaper.remove());
-}
+}*/
 
